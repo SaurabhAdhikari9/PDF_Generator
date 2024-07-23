a01546db3095632379674d21a1bf24411251687b
Saurabh Adhikari <saruravadhikari98@gmail.com>
2024-07-23 11:25:20 +0545

    custom text bold

--- PDFGenerator/PDFGenerator/DTO/PDFDTO.cs
+++ PDFGenerator/PDFGenerator/DTO/PDFDTO.cs
@@ -1,17 +1,31 @@
 ﻿using QuestPDF.Infrastructure;
+using System.Buffers.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace PDFGenerator.DTO
 {
     public class PDFDTO
     {
-        public PageHeader Header { get; set; }  
+        public PageHeader Header { get; set; }
+        public FooterDTO Footer { get; set; }
     }
 
     public class PageHeader
     {
-        public required string Text { get; set;}
+        public required string Text { get; set; }
         public required string ColorHex { get; set; }
-        public int FontSize { get; set;}
-        public bool Bold { get; set;}   
+        [Range(1,int.MaxValue,ErrorMessage ="FontSize must be greater than 0")]
+        public int FontSize { get; set; }
+        public int Boldness { get; set; }
+        public string? Image { get; set; }
+        public float Height { get; set; }
+    }
+
+    public class FooterDTO:PageHeader
+    {
+    }
+    public class Font
+    {
+        public required string FontFamily { get; set;}
     }
 }
---
+++ PDFGenerator/PDFGenerator/Helper/FontHelper/FontHandler.cs
@@ -0,0 +1,58 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+
+namespace PDFGenerator.Helper.FontHelper
+{
+    public static class FontHandler
+    {
+        public static TextStyle Boldness(int boldness = 0)
+        {
+            
+            if (boldness == 0)
+            {
+                boldness = 400; // equivalent to css 400 bold
+            }
+            else if (boldness <= 100)
+            {
+                boldness = 100;
+            }
+            else if (boldness <= 200)
+            {
+                boldness = 200;
+            }
+            else if (boldness <= 300)
+            {
+                boldness = 300;
+            }
+            else if(boldness <= 400)
+            {
+                boldness = 400;
+            }
+            else if( boldness <= 500)
+            {
+                boldness = 500;
+            }
+            else if(boldness <= 600)
+            {
+                boldness = 600;  
+            }
+            else if (boldness <= 700)
+            {
+                boldness = 700;
+            }
+            else if(boldness <= 800)
+            {
+                boldness = 800;
+            }
+            else if(boldness <= 900)
+            {
+                boldness = 900;
+            }
+            else if(boldness <= 1000)
+            {
+                boldness = 1000;
+            }
+            return TextStyle.Default.Weight((FontWeight)boldness);
+        }
+    }
+}
